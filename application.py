be59c7ce1050cfb3f730e2695d9fc20bc8cc02e1
Marc Zalik <marc82490@gmail.com>
2017-06-17 12:18:58 -0500

    updating flask app

--- application.py
+++ application.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+from flask_session import Session
+from tempfile import mkdtemp
 
 app = Flask(__name__)
+
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-if "name" == "__main__":
-    app.run()
+# @app.route('/')
+
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
